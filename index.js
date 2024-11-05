c87db90802057ba4c476990905c2a1b99f0ba059
Shivam Shukla <shivamshukla8103@gmail.com>
2024-11-05 11:53:16 +0530

    files added by shivam

---
+++ index.js
@@ -0,0 +1,10 @@
+
+        // Toggle mobile menu
+        const menuBtn = document.getElementById('menu-btn');
+        const mobileMenu = document.getElementById('mobile-menu');
+        
+        menuBtn.addEventListener('click', () => {
+          mobileMenu.classList.toggle('hidden');
+        });
+        // <button class="button" style="--clr:#006400;" onclick="window.location.href='donate.html'">Donate Now</button>
+
